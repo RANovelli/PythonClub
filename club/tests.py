7bba46720925710ed1d859952b6babb48f058367
ryana <ryan.a.novelli@gmail.com>
2021-05-24 20:21:52 -0700

    Model test commit

--- club/tests.py
+++ club/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Resource, Meeting
+import datetime
 
 # Create your tests here.
+class ResourceTest(TestCase):
+    def setUp(self):
+        self.type=Resource(resourcename='Python Tutorial')
+    
+    def test_resourcestring(self):
+        self.assertEqual(str(self.type), 'Python Tutorial')
+
+    def test_table(self):
+        self.assertEqual(str(Resource._meta.db_table), 'club_resource')
+
+class MeetingTest(TestCase):
+    def setUp(self):
+        self.type=Meeting(meetingtitle='Python Intro')
+        self.meeting=Meeting(meetingtitle='Intro', meetingdate=datetime.date(2021,5,24), meetingtime='5:00', meetinglocation='Vivace', meetingagenda='Python')
+    
+    def test_meetingstring(self):
+        self.assertEqual(str(self.type), 'Python Intro')
+
+    def test_table(self):
+        self.assertEqual(str(Meeting._meta.db_table), 'club_meeting')
